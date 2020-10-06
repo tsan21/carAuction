f5b1b196c78a3460b81f1da9460f53b1ccfe0b68
Tsan <t.wong@student.fontys.nl>
2020-10-06 15:04:59 +0200

    hoi

---
+++ src/main/java/s4/carauction/CarauctionApplication.java
@@ -0,0 +1,13 @@
+package s4.carauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarauctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarauctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s4/carauction/CarauctionApplicationTests.java
@@ -0,0 +1,13 @@
+package s4.carauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarauctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
