5a46108c4c5f9b9b1ccdad51225aa0f658180d54
tsan <tsanyip@hotmail.com>
2021-01-17 09:18:46 +0100

    test controller

---
+++ src/main/java/s4/carauction/controllers/TestController.java
@@ -0,0 +1,16 @@
+package s4.carauction.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
+public class TestController {
+
+    @RequestMapping("/")
+    public String test(){
+        return "this is car auction";
+    }
+}
